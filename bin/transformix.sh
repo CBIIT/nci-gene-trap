02f4c09052a74b8418e492c2653f30bb26b007e0
George Zaki <george.zaki@nih.gov>
2016-08-23 10:24:34 -0400

    scripts to call elastix and transformix from python on MAC

---
+++ bin/transformix.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Usage:transformix.sh <elastix arguments>  [log-file-path]
+
+if [ -z "$1"  ] 
+then
+  echo "Error: No input parameters to run transformix "  1>&2
+  exit 1
+fi
+
+if ! [ -z "$2" ]
+then 
+  OUT_LOG_FILE="$2"/log-out.txt 
+  ERR_LOG_FILE="$2"/log-err.txt 
+else
+  OUT_LOG_FILE=log-out.txt 
+  ERR_LOG_FILE=log-err.txt 
+fi
+
+
+if [ -z "ELASTIX_PATH" ]
+then
+  echo "Error: The environment variable ELASTIX_PATH is not set" 1>&2
+  exit 1
+fi
+
+
+export DYLD_LIBRARY_PATH="$ELASTIX_PATH:$DYLD_LIBRARY_PATH"
+
+transformix $1   >$OUT_LOG_FILE 2>$ERR_LOG_FILE
