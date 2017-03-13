e62a1b7e4b5a37157f86f121e20c9b3c8b92bbfb
George Zaki <george.zaki@nih.gov>
2017-03-13 15:09:33 -0400

    Follow the new convention of having the images directories and the csv files in the same level

--- utils/generate-swarm.py
+++ utils/generate-swarm.py
@@ -6,7 +6,7 @@ import os
 
 parser = argparse.ArgumentParser(description="A script to generate configuration files for cells, and to generate a swarm that can run on Biowulf.")
 
-parser.add_argument('cells_dir', help="The top level directory that contains the cells directories")
+parser.add_argument('cells_dir', help="The top level directory that contains the cells images directories, and the corresponding segmented rna.csv. The rna file should have the same prefix as the cell images directory.")
 parser.add_argument('input_master', help="The generic configuration file")
 parser.add_argument('-o', '--overwrite', action="store_true", help="Overwirte the configuration files if they exist.")
 
@@ -46,15 +46,28 @@ except ConfigParser.Error as error:
 cells = [x for x in os.listdir(cells_dir) if os.path.isdir(x)]
 
 
+#This is the top level tracking direcotries for all the cells.
+base_tracking_dir = config.get('arguments', 'processing-dir')
+
 print "found {0} cell directories".format(len(cells))
 #Modify the config file to include the cell directory
 processing_dirs=[]
 for cell_name in cells: 
 
+    #I should find a corresponding <cell_name>.csv file    
     #These names will come from the conventions
-    stack_dir = os.path.abspath(os.path.join(cells_dir, cell_name + '/cell-' + cell_name))
-    segmented_rnas = os.path.abspath(os.path.join(cells_dir, cell_name + '/cell-' + cell_name + '.csv'))
-    tracking_dir = os.path.abspath(os.path.join(cells_dir, cell_name + '/cell-' + cell_name + '-processing'))
+    stack_dir = os.path.abspath(os.path.join(cells_dir, cell_name))
+    segmented_rnas = os.path.abspath(os.path.join(cells_dir, cell_name + '.csv'))
+    tracking_dir = os.path.abspath(os.path.join(base_tracking_dir, cell_name + '-processing'))
+
+
+    if not os.path.exists(segmented_rnas):
+      print "WARNING: Can not find the segmented rnas files {0}".format(segmented_rnas)
+      continue
+
+
+    if not os.path.exists(tracking_dir):
+      os.mkdir(tracking_dir) 
 
     #Get the number of frames for the cell:
     image_prefix = config.get('arguments', 'images-prefix')
@@ -71,8 +84,7 @@ for cell_name in cells:
     config.set('arguments', 'destination', destination_frame)
     config.set('arguments', 'memory', n_frames)
 
-  
-    config_path = os.path.join(cells_dir, cell_name + "/config.cfg")
+    config_path = os.path.join(tracking_dir, "config.cfg")
     if not args.overwrite:
       if os.path.isfile(config_path):
         raise Exception ('Error, the configuration file "{0}" already exists'.format(config_path))
@@ -80,9 +92,8 @@ for cell_name in cells:
     with open(config_path, 'wb') as configfile:
         config.write(configfile)
 
-    new_job = os.path.join(cells_dir, cell_name)
-    processing_dirs.append(os.path.join(cells_dir, cell_name))
-    print "Added  {0} to the joblist".format(new_job)
+    processing_dirs.append(tracking_dir)
+    print "Added  {0} to the joblist".format(tracking_dir)
    
 
 #Generate a swarm file:
